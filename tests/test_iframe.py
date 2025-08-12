697491f66f978a530835e2e0fe1f802ec7f56291
FayHosseini-GLI <a.hosseini@gaminglabs.com>
2025-08-12 11:48:29 +1000

    Fix Chrome driver session error by deleting conftest file

--- tests/test_iframe.py
+++ tests/test_iframe.py
@@ -1,4 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 from pages.iframe_page import IframePage
+
+@pytest.fixture
+def driver():
+    options = Options()
+    #options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    yield driver
+    driver.quit()
 
 def test_internal_button_click(driver):     #receives a Selenium driver fixture (usually from conftest.py)
     page = IframePage(driver)               #creates an IframePage object, passing the driver so the page object can control the browser
